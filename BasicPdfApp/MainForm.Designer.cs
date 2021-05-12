d99502b72e8c0c25d0eea087617317353adee8bf
Heriklys Andrade <heriklys.andrade@totvs.com.br>
2021-05-12 16:46:41 -0300

    Add project files.

---
+++ BasicPdfApp/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace BasicPdfApp
+{
+  partial class MainForm
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+      this.btnNovoPdf = new System.Windows.Forms.Button();
+      this.fbdCaminhoPasta = new System.Windows.Forms.FolderBrowserDialog();
+      this.btnEditPdf = new System.Windows.Forms.Button();
+      this.ofdAbrirArquivo = new System.Windows.Forms.OpenFileDialog();
+      this.btnCopiaPdf = new System.Windows.Forms.Button();
+      this.lbKeyFormFields = new System.Windows.Forms.ListBox();
+      this.lbValueFormFields = new System.Windows.Forms.ListBox();
+      this.btnCombPdf = new System.Windows.Forms.Button();
+      this.btnCompriPdf = new System.Windows.Forms.Button();
+      this.btnProtPdf = new System.Windows.Forms.Button();
+      this.btnConverHtml = new System.Windows.Forms.Button();
+      this.btnConverDoc = new System.Windows.Forms.Button();
+      this.tbConverHtml = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // btnNovoPdf
+      // 
+      resources.ApplyResources(this.btnNovoPdf, "btnNovoPdf");
+      this.btnNovoPdf.Name = "btnNovoPdf";
+      this.btnNovoPdf.UseVisualStyleBackColor = true;
+      this.btnNovoPdf.Click += new System.EventHandler(this.btnNovoPdf_Click);
+      // 
+      // btnEditPdf
+      // 
+      resources.ApplyResources(this.btnEditPdf, "btnEditPdf");
+      this.btnEditPdf.Name = "btnEditPdf";
+      this.btnEditPdf.UseVisualStyleBackColor = true;
+      this.btnEditPdf.Click += new System.EventHandler(this.btnEditPdf_Click);
+      // 
+      // btnCopiaPdf
+      // 
+      resources.ApplyResources(this.btnCopiaPdf, "btnCopiaPdf");
+      this.btnCopiaPdf.Name = "btnCopiaPdf";
+      this.btnCopiaPdf.UseVisualStyleBackColor = true;
+      this.btnCopiaPdf.Click += new System.EventHandler(this.btnCopiaPdf_Click);
+      // 
+      // lbKeyFormFields
+      // 
+      this.lbKeyFormFields.FormattingEnabled = true;
+      resources.ApplyResources(this.lbKeyFormFields, "lbKeyFormFields");
+      this.lbKeyFormFields.Name = "lbKeyFormFields";
+      // 
+      // lbValueFormFields
+      // 
+      this.lbValueFormFields.FormattingEnabled = true;
+      resources.ApplyResources(this.lbValueFormFields, "lbValueFormFields");
+      this.lbValueFormFields.Name = "lbValueFormFields";
+      // 
+      // btnCombPdf
+      // 
+      resources.ApplyResources(this.btnCombPdf, "btnCombPdf");
+      this.btnCombPdf.Name = "btnCombPdf";
+      this.btnCombPdf.UseVisualStyleBackColor = true;
+      this.btnCombPdf.Click += new System.EventHandler(this.btnCombPdf_Click);
+      // 
+      // btnCompriPdf
+      // 
+      resources.ApplyResources(this.btnCompriPdf, "btnCompriPdf");
+      this.btnCompriPdf.Name = "btnCompriPdf";
+      this.btnCompriPdf.UseVisualStyleBackColor = true;
+      this.btnCompriPdf.Click += new System.EventHandler(this.btnCompriPdf_Click);
+      // 
+      // btnProtPdf
+      // 
+      resources.ApplyResources(this.btnProtPdf, "btnProtPdf");
+      this.btnProtPdf.Name = "btnProtPdf";
+      this.btnProtPdf.UseVisualStyleBackColor = true;
+      this.btnProtPdf.Click += new System.EventHandler(this.btnProtPdf_Click);
+      // 
+      // btnConverHtml
+      // 
+      resources.ApplyResources(this.btnConverHtml, "btnConverHtml");
+      this.btnConverHtml.Name = "btnConverHtml";
+      this.btnConverHtml.UseVisualStyleBackColor = true;
+      this.btnConverHtml.Click += new System.EventHandler(this.btnConverHtml_Click);
+      // 
+      // btnConverDoc
+      // 
+      resources.ApplyResources(this.btnConverDoc, "btnConverDoc");
+      this.btnConverDoc.Name = "btnConverDoc";
+      this.btnConverDoc.UseVisualStyleBackColor = true;
+      // 
+      // tbConverHtml
+      // 
+      resources.ApplyResources(this.tbConverHtml, "tbConverHtml");
+      this.tbConverHtml.Name = "tbConverHtml";
+      // 
+      // MainForm
+      // 
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.tbConverHtml);
+      this.Controls.Add(this.btnConverDoc);
+      this.Controls.Add(this.btnConverHtml);
+      this.Controls.Add(this.btnProtPdf);
+      this.Controls.Add(this.btnCompriPdf);
+      this.Controls.Add(this.btnCombPdf);
+      this.Controls.Add(this.lbValueFormFields);
+      this.Controls.Add(this.lbKeyFormFields);
+      this.Controls.Add(this.btnCopiaPdf);
+      this.Controls.Add(this.btnEditPdf);
+      this.Controls.Add(this.btnNovoPdf);
+      this.Name = "MainForm";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnNovoPdf;
+    private System.Windows.Forms.FolderBrowserDialog fbdCaminhoPasta;
+    private System.Windows.Forms.Button btnEditPdf;
+    private System.Windows.Forms.OpenFileDialog ofdAbrirArquivo;
+    private System.Windows.Forms.Button btnCopiaPdf;
+    private System.Windows.Forms.ListBox lbKeyFormFields;
+    private System.Windows.Forms.ListBox lbValueFormFields;
+    private System.Windows.Forms.Button btnCombPdf;
+    private System.Windows.Forms.Button btnCompriPdf;
+    private System.Windows.Forms.Button btnProtPdf;
+    private System.Windows.Forms.Button btnConverHtml;
+    private System.Windows.Forms.Button btnConverDoc;
+    private System.Windows.Forms.TextBox tbConverHtml;
+  }
+}
+
---
+++ BasicPdfApp/MainForm.cs
@@ -0,0 +1,197 @@
+﻿using System.Windows.Forms;
+using APToolkitNET;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Linq;
+using APDocConverter;
+
+namespace BasicPdfApp
+{
+  public partial class MainForm : Form
+  {
+    const float INCH = 72f;
+
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    #region Auxiliares
+
+    private string GetFilePath(OpenFileDialog fileDialog)
+    {
+      return fileDialog.FileName.TrimEnd(fileDialog.SafeFileName.ToCharArray());
+    }
+
+    private string GetNewFileName(OpenFileDialog fileDialog, FileNameOptionEnum nameOptionEnum)
+    {
+      switch (nameOptionEnum)
+      {
+        case FileNameOptionEnum.Edit:
+          return this.GetFilePath(fileDialog) + $"{ofdAbrirArquivo.SafeFileName.TrimEnd(".pdf".ToCharArray())}_edit_{new Random().Next(0, 200)}.pdf";
+
+        case FileNameOptionEnum.Copy:
+          return this.GetFilePath(fileDialog) + $"{ofdAbrirArquivo.SafeFileName.TrimEnd(".pdf".ToCharArray())}_copy_{new Random().Next(0, 200)}.pdf";
+
+        case FileNameOptionEnum.Merge:
+          return this.GetFilePath(fileDialog) + $"{ofdAbrirArquivo.SafeFileName.TrimEnd(".pdf".ToCharArray())}_merge_{new Random().Next(0, 200)}.pdf";
+
+        case FileNameOptionEnum.Encrypt:
+          return this.GetFilePath(fileDialog) + $"{ofdAbrirArquivo.SafeFileName.TrimEnd(".pdf".ToCharArray())}_encrypt_{new Random().Next(0, 200)}.pdf";
+
+        case FileNameOptionEnum.Compress:
+          return this.GetFilePath(fileDialog) + $"{ofdAbrirArquivo.SafeFileName.TrimEnd(".pdf".ToCharArray())}_compress_{new Random().Next(0, 200)}.pdf";
+
+        case FileNameOptionEnum.Convert:
+          return this.GetFilePath(fileDialog) + $"{ofdAbrirArquivo.SafeFileName.TrimEnd(".pdf".ToCharArray())}_convert_{new Random().Next(0, 200)}.pdf";
+
+        default:
+          return string.Empty;
+      }
+    }
+
+    #endregion
+
+    private void btnNovoPdf_Click(object sender, System.EventArgs e)
+    {
+      using (Toolkit oTK = new Toolkit
+      {
+        // Set the PDF page Height and Width (72 = 1")
+        OutputPageHeight = 11f * INCH,
+        OutputPageWidth = 8.5f * INCH
+      })
+      {
+
+        fbdCaminhoPasta.ShowDialog();
+
+        int intOpenOutputFile = oTK.OpenOutputFile(fbdCaminhoPasta.SelectedPath + "\\new.pdf");
+
+        // Each time a new page is required call NewPage
+        oTK.NewPage();
+
+        // Get the current version of Toolkit and save it to print on the PDF
+        string tkVer = oTK.ToolkitVersion;
+
+        oTK.SetFont("Helvetica", 24);
+        oTK.PrintText(INCH, 10f * INCH, $"Hello! Version {tkVer}");
+        oTK.PrintText(INCH, 9.5f * INCH, $"Date: {DateTime.Now}");
+
+        oTK.PrintJPEG(fbdCaminhoPasta.SelectedPath + "\\autotest.jpg", INCH, 8 * INCH, 110.0f, 107.0f, true);
+        oTK.CloseOutputFile();
+      }
+    }
+
+    private void btnCopiaPdf_Click(object sender, EventArgs e)
+    {
+      ofdAbrirArquivo.ShowDialog();
+
+      using (Toolkit oTK = new Toolkit())
+      {
+        oTK.OpenOutputFile(this.GetNewFileName(ofdAbrirArquivo, FileNameOptionEnum.Copy));
+        oTK.OpenInputFile(ofdAbrirArquivo.FileName);
+
+        oTK.CopyForm(0, 0);
+        oTK.CloseOutputFile();
+      }
+    }
+
+    private void btnEditPdf_Click(object sender, EventArgs e)
+    {
+      ofdAbrirArquivo.ShowDialog();
+
+      using (Toolkit oTK = new Toolkit())
+      {
+        oTK.OpenOutputFile(this.GetNewFileName(ofdAbrirArquivo, FileNameOptionEnum.Edit));
+        oTK.OpenInputFile(ofdAbrirArquivo.FileName);
+
+        BindingList<object> blFormFieldValues = new BindingList<object>();
+
+        foreach (DictionaryEntry item in oTK.GetInputFields())
+        {
+          lbKeyFormFields.Items.Add(item.Key);
+          blFormFieldValues.Add(item.Value);
+        }
+
+        oTK.DoFormFormatting = 1;
+        oTK.FormNumbering = 1;
+        oTK.SetFormFieldData("topmostSubform[0].Page1[0].f1_1[0]", "Jeremy Likness", 1);
+        //oTK.SetFormFieldData("topmostSubform[0].Page1[0].Address[0].f1_7[0]", "28202 Cabot Rd Ste 155", 1);
+        //oTK.SetFormFieldData("topmostSubform[0].Page1[0].Address[0].f1_8[0]", "Laguna Niguel, CA 92677", 1);
+
+        oTK.CopyForm(0, 0);
+
+        blFormFieldValues.ToList().ForEach(x => lbValueFormFields.Items.Add(x));
+
+        oTK.CloseOutputFile();
+      }
+    }
+
+    private void btnCombPdf_Click(object sender, EventArgs e)
+    {
+      fbdCaminhoPasta.ShowDialog();
+
+      using (Toolkit oTK = new Toolkit())
+      {
+        oTK.OpenOutputFile(fbdCaminhoPasta.SelectedPath + "\\teste_merge.pdf");
+        oTK.MergeFile(fbdCaminhoPasta.SelectedPath + "\\new.pdf", 0, 0);
+        oTK.MergeFile(fbdCaminhoPasta.SelectedPath + "\\fw9.pdf", 0, 0);
+        oTK.MergeFile(fbdCaminhoPasta.SelectedPath + "\\CertificadoCODEC.pdf", 0, 0);
+        oTK.CloseOutputFile();
+      }
+    }
+
+    private void btnProtPdf_Click(object sender, EventArgs e)
+    {
+      ofdAbrirArquivo.ShowDialog();
+
+      using (Toolkit oTK = new Toolkit())
+      {
+        oTK.EncryptPDF(5, ofdAbrirArquivo.FileName, this.GetNewFileName(ofdAbrirArquivo, FileNameOptionEnum.Encrypt), "senhateste", "senhateste", false, false, false, false);
+      }
+    }
+
+    private void btnCompriPdf_Click(object sender, EventArgs e)
+    {
+      ofdAbrirArquivo.ShowDialog();
+
+      using (Toolkit oTK = new Toolkit())
+      {
+        oTK.OpenInputFile(ofdAbrirArquivo.FileName);
+        
+        var compressor = oTK.GetCompressor();
+        compressor.CompressImages = true;
+        compressor.CompressObjects = true;
+        compressor.CompressionQuality = 20;
+        compressor.Activate();
+
+        oTK.OpenOutputFile(this.GetNewFileName(ofdAbrirArquivo, FileNameOptionEnum.Compress));
+
+        oTK.CopyForm(0, 0);
+
+        oTK.CloseOutputFile();
+      }
+    }
+
+    private void btnConverHtml_Click(object sender, EventArgs e)
+    {
+      ofdAbrirArquivo.ShowDialog();
+
+      DocConverter oDC = new DocConverter();
+
+      var result = oDC.ConvertToPDF(ofdAbrirArquivo.FileName, this.GetNewFileName(ofdAbrirArquivo, FileNameOptionEnum.Convert));
+
+      MessageBox.Show(result.DocConverterStatus.ToString());
+    }
+  }
+
+  public enum FileNameOptionEnum : int
+  {
+    Edit = 1,
+    Copy = 2,
+    Merge = 3,
+    Encrypt = 4,
+    Compress = 5,
+    Convert = 6
+  }
+}
